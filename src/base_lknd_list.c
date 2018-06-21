e04782e9efd34333743ba471074aa0e15dad2975
Duboisse Lucas <dlucas@localhost.localdomain>
2018-06-21 19:07:48 +0200

    coding style

--- src/base_lknd_list.c
+++ src/base_lknd_list.c
@@ -75,36 +75,3 @@ int add_meeting(meeting_t *meeting, char **tab)
 	tmp->next = new;
 	return (0);
 }
-
-int meeting_print(meeting_t *first)
-{
-	meeting_t *tmp = first;
-
-	while (tmp) {
-		printf("******************************\n");
-		printf("MEETING\n");
-		printf("id = %s\n", tmp->id);
-		printf("date = %s\n", tmp->date);
-		printf("place = %s\n", tmp->place);
-		tab_print(tmp->pers);
-		tmp = tmp->next;
-	}
-	return (0);
-}
-
-int employee_print(employee_t *first)
-{
-	employee_t *tmp = first;
-
-	while (tmp) {
-		printf("******************************\n");
-		printf("EMPLOYEE\n");
-		printf("first_name = %s\n", tmp->first_name);
-		printf("last_name = %s\n", tmp->last_name);
-		printf("position = %s\n", tmp->position);
-		printf("city = %s\n", tmp->city);
-		printf("id = %s\n", tmp->city);
-		tmp = tmp->next;
-	}
-	return (0);
-}
---
+++ src/print.c
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2018
+** Duboisse Lucas
+** File description:
+** Print
+*/
+
+#include <stdio.h>
+#include "calendar.h"
+
+int meeting_print(meeting_t *first)
+{
+	meeting_t *tmp = first;
+
+	while (tmp) {
+		printf("******************************\n");
+		printf("MEETING\n");
+		printf("id = %s\n", tmp->id);
+		printf("date = %s\n", tmp->date);
+		printf("place = %s\n", tmp->place);
+		tab_print(tmp->pers);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int employee_print(employee_t *first)
+{
+	employee_t *tmp = first;
+
+	while (tmp) {
+		printf("******************************\n");
+		printf("EMPLOYEE\n");
+		printf("first_name = %s\n", tmp->first_name);
+		printf("last_name = %s\n", tmp->last_name);
+		printf("position = %s\n", tmp->position);
+		printf("city = %s\n", tmp->city);
+		printf("id = %s\n", tmp->city);
+		tmp = tmp->next;
+	}
+	return (0);
+}
