73ed103d8fc257dd902e73531983bf7305b6e9f0
Duboisse Lucas <dlucas@localhost.localdomain>
2018-06-21 10:02:10 +0200

    requirement

---
+++ requirement.c
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2018
+** Lucas Duboisse
+** File description:
+** requirement
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct list
+{
+	void *data;
+	struct list *next;
+} list_t;
+
+int my_revlist_synthesis(list_t **begin)
+{
+	list_t *next = NULL;
+	list_t *prev = NULL;
+	list_t *tmp = *begin;
+
+	if (*begin == NULL)
+		return (84);
+	while (tmp != NULL) {
+		next = tmp->next;
+		tmp->next = prev;
+		prev = tmp;
+		tmp = next; 
+	}
+	*begin = prev;
+	return (0);
+}
+
+list_t *my_findnodeeqinlist_synthesis(list_t *begin, void *data_ref, 
+										int (*cmp)(void *, void *))
+{
+	list_t *tmp = begin;
+
+	if (begin == NULL)
+		return (NULL);
+	while (tmp != NULL) {
+		if (cmp(tmp->data, data_ref) == 0)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
